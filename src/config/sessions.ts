8a8f9471d733d873fa690e3cfd194f5f3059bdc3
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2024-06-06 14:48:42 +0000

    Setting up session management for backend

--- src/config/sessions.ts
+++ src/config/sessions.ts
@@ -1,7 +1,7 @@
 import session from "express-session";
 
 const sess = session({
-    secret: 'afrovivo@4567',
+    secret: 'rBwD183!@#EfGI456$%^pjKl789&*()MnOpQr%$&vWxY56',
     resave: false,
     saveUninitialized: false,
     cookie: {
