3dfecb3cb84f025a87ba7e83b0f392dd0c7741a5
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2024-06-13 16:10:15 +0000

    ...

--- src/routes/init.ts
+++ src/routes/init.ts
@@ -1,7 +1,7 @@
 import { Request, Response, Express } from "express";
 
 export function initRoutes(server:Express) {
-    server.get("./", (request:Request, response:Response) => {
+    server.get("/", (request:Request, response:Response) => {
         response.status(200).send("Welcome from Afrovivo");
     })
 }
