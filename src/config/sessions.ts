b6d911a838ef77d56d353a19a14100c8391fef94
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2024-06-14 16:44:49 +0000

    Using Redis for caching

--- src/config/sessions.ts
+++ src/config/sessions.ts
@@ -1,5 +1,7 @@
 import session from "express-session";
 import dotenv from "dotenv";
+import { createClient } from "redis";
+import RedisStore from "connect-redis";
  
 dotenv.config();
 const secretKey = process.env.SECRET_KEY as string
@@ -8,7 +10,16 @@ if (!secretKey) {
     throw new Error("No secret key found. Please define SECRET_KEY environment variable.");
 }
 
+const reidsClient = createClient();
+reidsClient.connect().catch(console.error);
+
+const redisStore = new RedisStore({
+    client: reidsClient,
+    prefix: 'afrovivo',
+})
+
 const sess = session({
+    store: redisStore,
     secret: secretKey,
     resave: false,
     saveUninitialized: false,
