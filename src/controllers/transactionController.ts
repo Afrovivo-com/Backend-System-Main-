7c32b06d3aa07a7082a7b0feb3486f7868b8e3ee
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2024-08-30 00:09:51 +0000

    ...

--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -1,6 +1,5 @@
 import  {Request, Response} from "express";
 import {createTransaction, getTransactionById, labelTransaction} from "../services/transactionLabelling";
-import {Status} from "@prisma/client"
 export const createTransactionController=async(req:Request, res:Response)=>
 {
     try{
